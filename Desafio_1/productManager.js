38e2d1050e8c3ea17dcfb2883f7146d8a0b688a2
Rubens Galarraga <rgalarraga121@gmail.com>
2022-12-02 08:57:29 -0300

    edit

--- Desafio_1/productManager.js
+++ Desafio_1/productManager.js
@@ -51,12 +51,12 @@ class ProductManager {
 
 const manager = new ProductManager();
 
-console.log(manager.getProducts()); //Devuelve array vacio de products
+console.log(manager.getProducts()); // Devuelve array vacio de products
 
 manager.addProduct('Test product', 'This is a test product', 200, 'No picture', 'abc123', 25)
 manager.addProduct('Second test product', 'This is a second test product', 100, 'No picture', 'abc124', 25)
 
-console.log(manager.getProducts()); //Devuelve el array con estos dos products agregados con sus respectivos IDs
+console.log(manager.getProducts()); // Devuelve el array con estos dos products agregados con sus respectivos IDs
 manager.addProduct('Test product', 'This is a test product', 200, 'No picture', 'abc123', 25) //No agrega este product porque coincide el code
-console.log(manager.getProductByID(2)) //Trae el product con ID 2
-manager.getProductByID(9) //No encuentra product con este ID
+console.log(manager.getProductByID(2)) // Trae el product con ID 2
+manager.getProductByID(9) // No encuentra product con este ID
